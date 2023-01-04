86ad02d8a25eabb1f7646b8c36dcdd6d1d355277
Alex Warnes <alex.s.warnes@gmail.com>
2023-01-04 13:19:29 -0600

    actually export things

--- src/lib/index.js
+++ src/lib/index.js
@@ -1,1 +1,10 @@
 // Reexport your entry components here
+import RangeSlider from "./components/RangeSlider.svelte";
+import RotationSliders from "./components/RotationSliders.svelte";
+import Vec3Input from "./components/Vec3Input.svelte";
+
+export {
+  RangeSlider,
+  RotationSliders,
+  Vec3Input,
+};
